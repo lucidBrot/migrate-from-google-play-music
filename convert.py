b597858dde9623b885d7b0556d275abcb106433c
Eric Mink <eric@mink.li>
2020-08-11 16:05:31 +0200

    consider only content

--- convert.py
+++ convert.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Python version 3
-import os, sys
+import os, sys, csv
+from dataclasses import dataclass
 
 # Full Path to "Takeout / Google Play Music / Playlists" as obtained from takeout.google.com
 PLAYLISTS_PATH = os.path.normpath('N:\Files\Backups\GPM_export\Takeout\Google Play Music\Playlists')
@@ -22,19 +23,57 @@ def filter_playlists(subfolders):
         if valid:
             yield(folder)
 
+@dataclass
+class SongInfo:
+    """
+      Basically a Named Tuple for storing info of a song
+    """
+    title: str
+    artist: str
+    liked: bool
+    album: str
+
+def read_gpm_playlist(playlistdir):
+    """
+      Returns a list of song names contained in a GPM Takeout Playlist
+    """
+    tracks_path = os.path.join(playlistdir, 'Tracks')
+    # Expected contents of that directory is one file per song, 
+    # each file csv-formatted and containing something like this:
+    # 
+    # Title,Album,Artist,Duration (ms),Rating,Play Count,Removed,Playlist Index
+    # "The Show","Lenka","Lenka","235728","5","24","","4"
+    # <newline>
+    
+    song_csvs = [ f.path for f in os.scandir(tracks_path) if f.is_file() ]
+    for song_csv in song_csvs:
+        with open(song_csv, encoding="utf-8") as csvfile:
+            rows = csv.reader(csvfile)
+            for title, album, artist, duration_ms, rating, play_count, removed, playlist_index in rows:
+                if (title.strip() == 'Title') and (artist.strip() == 'Artist') and (album.strip() == 'Album'):
+                    # skip headline
+                    continue
+                print("considering {} by {} in Album {}".format(title, artist, album))
+
 def print_todos():
     print("\n--- TODOS ---")
     print("\t handle the Thumbs Up playlist.")
+    print("\t check for surprising cases with more than two rows in a song csv")
 
 def main():
     print("Considering any playlists in {}".format(PLAYLISTS_PATH))
     
     print("Collecting playlist directories...\n")
     subfolders = [ f.path for f in os.scandir(PLAYLISTS_PATH) if f.is_dir() ]
-    playlists_generator = filter_playlists(subfolders)
-    for playlistpath in playlists_generator:
+    playlists = list(filter_playlists(subfolders))
+    for playlistpath in playlists:
         playlistname = os.path.basename(playlistpath)
         print("\tPlaylist: {}".format(playlistname))
+
+    print("Accumulating Contents...")
+    for playlistpath in playlists:
+        db = read_gpm_playlist(playlistpath)
+
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == 'help':
